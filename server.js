9ce18e87ba41e7815c40f988ab825100ad67cc17
ricard0o <71363034+ricard0o@users.noreply.github.com>
2021-01-27 10:53:32 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
-var express = require('express');
-var app     = express();
+const express = require('express');
+const path = require('path');
+const app = express();
 
-app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/dist/'));
+app.use('/src/assets', express.static(__dirname + '/src/assets/'));
 
-//For avoidong Heroku $PORT error
-app.get('/', function(request, response) {
-    var result = 'App is running'
-    response.send(result);
-}).listen(app.get('port'), function() {
-    console.log('App is running, server is listening on port ', app.get('port'));
-});
+
+app.listen(process.env.PORT || 8080);
