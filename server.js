3ed40bc56c04f548b66f7986b2b60a134fc2ceda
ricard0o <71363034+ricard0o@users.noreply.github.com>
2021-01-27 10:58:33 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,37 @@
-const express = require('express');
-const path = require('path');
-const app = express();
+const express = require('express')
+const app = express()
+const port = 4000
+const path = require('path')
+const router = express.Router()
 
-app.use(express.static(__dirname + '/dist/'));
-app.use('/src/assets', express.static(__dirname + '/src/assets/'));
 
+app.use(express.static('public'))
+//app.use('/src', express.static(__dirname + '/src/js'))
 
-app.listen(process.env.PORT || 8080);
+/*app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/docs/src/index.html')
+}) */
+
+router.get('/', function(req, res) {
+    res.sendFile(__dirname + '/public/index.html')
+});
+router.get('/about.html', function(req, res)  {
+    res.sendFile(path.join(__dirname + '/public/about.html'))
+})
+
+router.get('/index.html', function(req, res)  {
+    res.sendFile(path.join(__dirname + '/public/index.html'))
+})
+
+router.get('/documentation.html', function(req, res)  {
+    res.sendFile(path.join(__dirname + '/public/documentation.html'))
+})
+
+router.get('/', function(req, res)  {
+    res.sendFile(path.join(__dirname + '/docs/css/styles.css'))
+})
+
+app.use('/', router);
+app.listen(process.env.PORT || 3000);
+
+/*app.listen(port, () => console.info('App listening on port ${port}'))*/
